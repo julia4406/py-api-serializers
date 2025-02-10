fb4b3a5ff551ce49ee0135b30a3bb7b5e1af6639
Julia Alexeeva <julia4406@gmail.com>
2025-02-10 16:38:05 +0200

    Removed debug_toolbar

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
     "rest_framework",
     "cinema",
     "user",
@@ -51,7 +50,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
